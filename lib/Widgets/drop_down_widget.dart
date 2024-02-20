c2da207e7dc4734e4392a7df258dcd2ff0978a50
Huzaifa <huzaifa@ambeego.com>
2024-02-20 23:15:39 +0500

    custom drop down to handle any type of list

---
+++ lib/Widgets/drop_down_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomizeDropDown extends StatefulWidget {
+  final List<DropdownMenuItem<dynamic>> items;
+  dynamic selectedValue;
+
+  CustomizeDropDown({Key? key, required this.items}) : super(key: key);
+
+  @override
+  State<CustomizeDropDown> createState() => _CustomizeDropDownState();
+}
+
+class _CustomizeDropDownState extends State<CustomizeDropDown> {
+  @override
+  Widget build(BuildContext context) => Container(
+        width: MediaQuery.of(context).size.width * 0.40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(
+            color: Theme.of(context).colorScheme.primary
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 2, right: 2),
+          child: DropdownButton<dynamic>(
+            underline: const SizedBox(),
+            value: widget.items.contains(widget.selectedValue)
+                ? widget.selectedValue
+                : null,
+            isExpanded: true,
+            icon: Icon(Icons.arrow_drop_down_rounded,
+                size: 32, color: Theme.of(context).colorScheme.primary),
+            hint: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(widget.selectedValue ?? 'Select'),
+            ),
+            elevation: 16,
+            dropdownColor: Colors.white,
+            items: widget.items,
+            onChanged: (newValue) =>
+                setState(() => widget.selectedValue = newValue),
+          ),
+        ),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Widgets/drop_down_widget.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
-      ),
-      home: const MyHomePage(title: 'Flutter reusable Widgets'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true),
+        home: MyHomePage(title: 'Flutter Widgets'),
+      );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+  MyHomePage({super.key, required this.title});
   final String title;
+  List sampleItems = ['Option 1', 'Option 2', 'Option 3'];
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: CustomizeDropDown(
+            items: widget.sampleItems
+                .map(
+                  (item) => DropdownMenuItem<String>(
+                    value: item.toString(),
+                    child: Text(
+                      item.toString(),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      );
 }
